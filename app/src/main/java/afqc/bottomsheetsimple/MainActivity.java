5fb4e5fff0f6e487682a60d3860aedb085edfc16
AlexFrancoQC <afqcdeveloper@gmail.com>
2020-09-17 12:26:23 -0300

    Initial commit

---
+++ app/src/main/java/afqc/bottomsheetsimple/MainActivity.java
@@ -0,0 +1,14 @@
+package afqc.bottomsheetsimple;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
